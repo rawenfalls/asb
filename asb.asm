1917a644724be79c51e89f193ef40072bc655af6
rawenfalls <33192916+rawenfalls@users.noreply.github.com>
2022-05-24 21:44:26 +0300

    Update asb.asm

--- asb.asm
+++ asb.asm
@@ -25,7 +25,10 @@
         mov [0x04F4], 0x0;count even
         mov [0x04F2], 0x0;count odd
         mov [0x04F0], 0x0;count arithmetic mean odd
-        mov [0x04EE], 0x0 
+        mov [0x04EE], 0x0
+        
+        mov [0x04EC], 0x0;buffer for even
+        mov [0x04EA], 0x0 
          
         mov di, [0x04FC]
         mov dx, 0
@@ -55,8 +58,77 @@
             idiv cx
             mov [0x04F0],ax
             mov [0x04EE],dx
+            
+            mov cx, [0x04F2]
+            mov si, [0x04F2]
+            sub cx, 1
+            mov di, [0x04F6];sort odd
+        w1: mov dx, [di]
+            add di, 2
+            mov ax, [di]
+            cmp dx, ax
+            jg swap             
         
-         
+        w0: loop w1
+            sub si, 1
+            cmp si, 0
+            je sort_even
+            mov cx,[0x04F2]
+            sub cx, 1
+            mov di, [0x04F6]
+            jmp w0
+    
+        swap: mov [di], dx
+              sub di,2
+              mov [di], ax  
+              add di,2
+              jmp w0
+       ;-----------------------------------------------       
+ sort_even: mov cx, [0x04F4]
+            mov si, [0x04F4]
+            sub cx, 1
+            mov di, [0x04F8];sort even
+        w3: mov dx, [di]
+            add di, 2
+            mov ax, [di]
+            mov [0x04EC], ax
+            mov [0x04EA], dx
+    getabs: neg ax
+            js getabs
+    getabs1:neg dx
+            js getabs1        
+            cmp dx, ax
+            jl swap1             
         
-        final:
-        
+        w2: loop w3
+            sub si, 1
+            cmp si, 0
+            je finish
+            mov cx,[0x04F4]
+            sub cx, 1
+            mov di, [0x04F8]
+            jmp w2
+    
+        swap1:mov ax,[0x04EC]
+              mov dx,[0x04EA]
+              mov [di], dx
+              sub di,2
+              mov [di], ax  
+              add di,2
+              jmp w2      
+              
+                   
+    
+        finish:
+    
+    
+    
+    
+;        cmp ax,0
+;        js q6 
+;        div [0x04F2]
+;        jmp finish_count_mean 
+;        q6:neg ax
+;        div [0x04F2]
+;        neg ax
+;        finish_count_mean:mov [0x04F0], ax    
